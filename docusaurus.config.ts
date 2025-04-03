6ff901a627ab53713c37ddc6cb0c380239f77498
Pedro Casaubon <xperiments@gmail.com>
2025-04-03 15:28:07 +0200

    Update publish paths

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   favicon: "img/favicon.webp",
 
   // Set the production url of your site here
-  url: "https://xperiments.in",
+  url: "https://tigertag-project.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/tigertag",
+  baseUrl: "/TigerTag-Doc",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "tigertag-project", // Usually your GitHub org/user name.
+  projectName: "tigertag-doc", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
